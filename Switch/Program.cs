06fdb679fa4a4d9f56e5810baca74b8b0c950dc8
daniela marques <dd978452@gmail.com>
2023-01-16 13:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Switch{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor = "rosa";
+//com switch nao pode trabalhar com operadores comparacionais exp: > < etc.
+            switch (cor)
+            {
+                case "vermelho":
+                    Console.WriteLine("sua cor favorita é vermelho!");
+                    break;
+                case "amarelo":
+                    Console.WriteLine("sua cor favorita é amarelo!");
+                    break;
+                case "azul":
+                    Console.WriteLine("sua cor favorita é azul!");
+                    break;
+                default: //se eu tiver uma variavel verde e em nenhum case tiver essa cor, ele vai executar o que estiver dentro do default.
+                         //default é bem semelhante com o "else"
+                    Console.WriteLine("sua cor favorita eu não sei!");
+                    break;
+            }
+
+
+        }
+    }
+}
